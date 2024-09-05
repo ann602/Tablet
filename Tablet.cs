fe2eefdcd90d21e85991eb9d54d017d6e383f4b6
777 <777@HOME-PC>
2024-09-05 19:38:14 +0300

    Добавьте файлы проекта.

---
+++ Tablet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+
+{
+    public class Tablet
+    {
+        public string brand;
+        public int price;
+        public int weight;
+        public string color;
+        public bool isthereasimcard;
+        public bool isthereamemorycardslot;
+        public double screendiagonal;
+        public double CPUfrequency;
+
+        public double discountedPrice()
+        {
+            double discountPercentage = 30;
+            double discountedPrice = price - (price * discountPercentage / 100);
+            return discountedPrice;
+        }
+
+        public double discountwitharegularcustomercard()
+        {
+            double discount = 5;
+            double discountwitharegularcustomercard = discountedPrice() - (discountedPrice() * discount / 100);
+            return discountwitharegularcustomercard;
+        }
+    }
+}
